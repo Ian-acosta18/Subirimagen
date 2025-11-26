1b96a3dcb922021b4fd4bbf0412949f17cd3d26c
Ian Acosta <ian.acostaoz@uppuebla.edu.mx>
2025-11-26 12:16:42 -0600

    version 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Este archivo va en la carpeta raíz, NO dentro de "app"
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
